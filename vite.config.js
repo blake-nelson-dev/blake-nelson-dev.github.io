c5832bf15c9300505efd4bf98164e14f7eec411b
bldane <nelsbl292@gmail.com>
2025-02-09 20:45:57 -0600

    changed vite again, removed website name

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/blake-nelson-dev.github.io/",
+  base: "/",
   css: {
     postcss: './postcss.config.js'
   }
