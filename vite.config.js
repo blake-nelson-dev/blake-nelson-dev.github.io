9962e0cf4bbc29f24514349472c33c06c0e1bf4f
bldane <nelsbl292@gmail.com>
2025-02-09 20:44:24 -0600

    changed vite, added repo name

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./", // This is important for GitHub Pages
+  base: "/blake-nelson-dev.github.io/",
   css: {
     postcss: './postcss.config.js'
   }
